30c32389696453d30cd78d79a6d62402d19f798d
David Garcia <dfgarciasdev@gmail.com>
2022-09-21 00:01:35 +0000

    Update env config

--- dynamo/dynamo.sh
+++ dynamo/dynamo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 aws cloudformation deploy \
 --region us-east-1 \
---parameter-overrides DynamoName="lablambda" DynamoKey="dni" \
---stack-name dynamo-lambda-lab \
+--parameter-overrides DynamoName="lablambdadavidgarcia" DynamoKey="dni" \
+--stack-name davidgarcia-dynamo-lambda-lab \
 --template-file ./dynamo.yml
--- prereq/aws-codepipeline-prereq.sh
+++ prereq/aws-codepipeline-prereq.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 aws cloudformation deploy \
 --region us-east-1 \
---stack-name codepipeline-lambda-prereq \
+--stack-name davidgarcia-codepipeline-lambda-prereq \
 --template-file ./aws-codepipeline-prereq.yml \
 --capabilities CAPABILITY_IAM \
 --capabilities CAPABILITY_NAMED_IAM
